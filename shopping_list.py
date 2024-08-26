f56f160326313717c36899edaa512fe1470ea6e7
Dawid Masiarek <dawid.masiarek@adres.pl>
2024-08-26 10:43:02 +0200

    Second change

--- shopping_list.py
+++ shopping_list.py
@@ -2,3 +2,7 @@ shopping_dict = {
   "piekarnia": ["chleb", "bułki", "pączek"],
   "warzywniak": ["marchew", "seler", "rukola"]
 }
+for shop, products in shopping_dict.items():
+  shop=shop.capitalize()
+  products=[product.capitalize() for product in products]
+  print(f"Idę do {shop} i kupuję tam {products}.")
