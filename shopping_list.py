367b1d8a15a028bd8508d4ce852cbe965b1b2fa1
Dawid Masiarek <dawid.masiarek@adres.pl>
2024-08-26 10:44:08 +0200

    Third change

--- shopping_list.py
+++ shopping_list.py
@@ -6,3 +6,4 @@ for shop, products in shopping_dict.items():
   shop=shop.capitalize()
   products=[product.capitalize() for product in products]
   print(f"Idę do {shop} i kupuję tam {products}.")
+  number_of_products = sum(len(products) for products in shopping_dict.values())
