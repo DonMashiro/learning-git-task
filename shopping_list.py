0f69199f093f689fddf4d576c52e26ecb82ffcf4
Dawid Masiarek <dawid.masiarek@adres.pl>
2024-08-26 10:45:47 +0200

    Print result

--- shopping_list.py
+++ shopping_list.py
@@ -7,3 +7,4 @@ for shop, products in shopping_dict.items():
   products=[product.capitalize() for product in products]
   print(f"Idę do {shop} i kupuję tam {products}.")
   number_of_products = sum(len(products) for products in shopping_dict.values())
+  print(f"W sumie kupuję {number_of_products} produktów.")
